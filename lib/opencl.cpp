c1c24e8be81a47407bc30bf474e2243b62905cc8
Igor Schukin <igor@df.rtu.lv>
2025-05-15 15:46:35 +0300

    Fix redundant assignment in OpenCL buffer creation

--- lib/opencl.cpp
+++ lib/opencl.cpp
@@ -150,7 +150,7 @@ void OpenCL::run(
             switch (type)
             {
                 case ArgTypes::IN_IBUF:
-                    buffer = buffer = clCreateBuffer(_context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(int) * size, value, &err);
+                    buffer = clCreateBuffer(_context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(int) * size, value, &err);
                     break;
                 case ArgTypes::OUT_IBUF:
                     buffer = clCreateBuffer(_context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(int) * size, NULL, &err);
