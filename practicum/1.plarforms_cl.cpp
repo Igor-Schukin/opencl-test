6cea540a7e355647fe14717f68eeb9a2743edeb5
Igor Schukin <igors@moderro.com>
2026-01-08 22:00:09 +0200

    Refactor error handling in platform info retrieval and improve output clarity

--- practicum/1.plarforms_cl.cpp
+++ practicum/1.plarforms_cl.cpp
@@ -2,39 +2,47 @@
 #include <CL/cl.h>
 
 int main() {
-    cl_int err;
+    cl_int error;
     cl_uint num_platforms;
 
     // Get platform count
-    err = clGetPlatformIDs(0, NULL, &num_platforms);
+    error = clGetPlatformIDs(0, NULL, &num_platforms);
     std::cout << "Platforms Count: " << num_platforms << std::endl;
-    std::cout << "Result/Error 1. Cl.GetPlatformIDs: " << err << std::endl;
+    std::cout << "Result/Error 1. Cl.GetPlatformIDs: " << error << std::endl;
 
     // Get all platforms
     cl_platform_id* platforms = new cl_platform_id[num_platforms];
-    err = clGetPlatformIDs(num_platforms, platforms, NULL);
-    std::cout << "Result/Error 2. Cl.GetPlatformIDs: " << err << std::endl;
+    error = clGetPlatformIDs(num_platforms, platforms, NULL);
+    std::cout << "Result/Error 2. Cl.GetPlatformIDs: " << error << std::endl;
     std::cout << "------------------------------------" << std::endl;
 
     // Iterate over each platform and get info
     for (cl_uint i = 0; i < num_platforms; i++) {
         char platform_name[128]{};
-        char platform_profile[128]{};
-        char platform_vendor[128]{};
-        char platform_version[128]{};
-        char platform_extensions[1024]{};
-        
-        err = clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, sizeof(platform_name), platform_name, NULL);
-        clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, sizeof(platform_profile), platform_profile, NULL);
-        clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, sizeof(platform_vendor), platform_vendor, NULL);
-        clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, sizeof(platform_version), platform_version, NULL);
-        clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, sizeof(platform_extensions), platform_extensions, NULL);
+        error = clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, sizeof(platform_name), platform_name, NULL);
+        if (error != CL_SUCCESS) std::cout << "Error getting platform name: " << error << std::endl;
+        else std::cout << "Platform Name: " << platform_name << std::endl;
 
-        std::cout << "Platform Name: " << platform_name << std::endl;
-        std::cout << "Platform Profile: " << platform_profile << std::endl;
-        std::cout << "Platform Vendor: " << platform_vendor << std::endl;
-        std::cout << "Platform Version: " << platform_version << std::endl;
-        std::cout << "Platform Extensions: " << platform_extensions << std::endl;
+        char platform_profile[128]{};
+        error = clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, sizeof(platform_profile), platform_profile, NULL);
+        if (error != CL_SUCCESS) std::cout << "Error getting platform profile: " << error << std::endl;
+        else std::cout << "Platform Profile: " << platform_profile << std::endl;
+
+        char platform_vendor[128]{};
+        error = clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, sizeof(platform_vendor), platform_vendor, NULL);
+        if (error != CL_SUCCESS) std::cout << "Error getting platform vendor: " << error << std::endl;
+        else std::cout << "Platform Vendor: " << platform_vendor << std::endl;
+
+        char platform_version[128]{};
+        error = clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, sizeof(platform_version), platform_version, NULL);
+        if (error != CL_SUCCESS) std::cout << "Error getting platform version: " << error << std::endl;
+        else std::cout << "Platform Version: " << platform_version << std::endl;
+
+        char platform_extensions[1024]{};
+        error = clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, sizeof(platform_extensions), platform_extensions, NULL);
+        if (error != CL_SUCCESS) std::cout << "Error getting platform extensions: " << error << std::endl;
+        else std::cout << "Platform Extensions: " << platform_extensions << std::endl;
+
         std::cout << "------------------------------------" << std::endl;
     }
 
