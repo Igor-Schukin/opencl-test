dbe7f8955a06c7aebe3fab81f02724155bba6b0e
Igor Schukin <igors@moderro.com>
2026-01-09 22:47:15 +0200

    Format kernel function parameters for improved readability

--- practicum/3.vector_cl.cpp
+++ practicum/3.vector_cl.cpp
@@ -51,9 +51,11 @@ cl_device_id clInit() {
 
 const char* vectorAddKernelSource = R"(
     __kernel void vector_add
+    (
         __global const float* A, 
         __global const float* B, 
-        __global float* C) 
+        __global float* C
+    ) 
     {
         int id = get_global_id(0);
         C[id] = A[id] + B[id];
